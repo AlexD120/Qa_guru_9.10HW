bd0d9990d99f0275b9c681203b4d2d65fd6f0b66
aliya <alexdavydov92@gmail.com>
2023-12-16 21:49:43 +0300

    Изменил разрешение экрана

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,8 @@ import pytest
 @pytest.fixture(scope='function', autouse=True)
 def browser_config():
     browser.config.base_url = 'https://demoqa.com'
-    browser.config.window_width = 1280
-    browser.config.window_height = 1024
+    browser.config.window_width = 1440
+    browser.config.window_height = 900
 
     yield
 
