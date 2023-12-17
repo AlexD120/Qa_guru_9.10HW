845f7227e159716cc166f989a04b040f0b6ac04e
aliya <alexdavydov92@gmail.com>
2023-12-17 13:35:16 +0300

    12

--- qa_guru_9_10HW/resource.py
+++ qa_guru_9_10HW/resource.py
@@ -2,4 +2,4 @@ from pathlib import Path
 
 
 def path(image_name):
-    return str(Path(__file__).parent.joinpath(f'qa_guru_9_10HW/image/{image_name}'))
+    return str(Path(__file__).parent.joinpath(f'image/{image_name}'))
